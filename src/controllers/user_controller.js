475f414c88f06ec539bd336a3d7b08609d342e8b
Productos-Union <manager@union.pe>
2022-11-21 16:14:46 -0500

    token

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -83,21 +83,6 @@ userCtr.singin = async (req, res) => {
   }
 };
 
-userCtr.createUser = async(req, res)=>{
-
-  try {
-      const{ usuario, password, id_rol } = req.body;
-      const password2 = await helpers.encryptPassword(password);
-      await pool.query('insert into usuario(usuario, password, id_rol) values($1,$2,$3)', [usuario, password2, id_rol]);
-      return res.status(200).json(
-          `Usuario ${ username } creado correctamente...!`);
-  } catch (e) {
-      console.log(e);
-      return res.status(500).json('Internal Server error...!');
-  }
-
-}
-
 (userCtr.autentication = verifyToken), (req, res) => {
   res.json("Informacion secreta");
 };
@@ -113,6 +98,21 @@ function verifyToken(req, res, next) {
   } else {
     res.status(401).json("Token vacio");
   }
+}
+
+userCtr.createUser = async(req, res)=>{
+
+  try {
+      const{ usuario, password, id_rol } = req.body;
+      const password2 = await helpers.encryptPassword(password);
+      await pool.query('insert into usuario(usuario, password, id_rol) values($1,$2,$3)', [usuario, password2, id_rol]);
+      return res.status(200).json(
+          `Usuario ${ username } creado correctamente...!`);
+  } catch (e) {
+      console.log(e);
+      return res.status(500).json('Internal Server error...!');
+  }
+
 }
 
 module.exports = userCtr;
