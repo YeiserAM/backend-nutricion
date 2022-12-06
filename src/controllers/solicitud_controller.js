70e091f917484c80c75fc5e4a65068cb1e361b90
ChristianHumpiri1 <christianquispe@upeu.edu.pe>
2022-12-06 11:53:15 -0500

    idd

--- src/controllers/solicitud_controller.js
+++ src/controllers/solicitud_controller.js
@@ -5,7 +5,7 @@ const solicitudctr = {};
 
 solicitudctr.getAllsolicitud = async (req, res) => {
     try {
-      const response = await pool.query("select p.nombre, concat(p.apepat,' ', p.apemat) as Apellidos,p.dni, p.codigo,e.telefono as telefonoe , e.ubigeo, e.nacionalidad, e.estadocivil,m.nombree, m.nombrerep,m.gradosup , m.cargorep ,m.areappp , m.telefono,m.fechappp  , m.direccion from empresa m inner join estudiante e  on e.idestudiante  = m.idestudian inner join persona p on p.idpersona = e.idperso  ");
+      const response = await pool.query("select  p.idpersona ,p.nombre, concat(p.apepat,' ', p.apemat) as Apellidos,p.dni, p.codigo, e.idestudiante ,e.telefono as telefonoe , e.ubigeo, e.nacionalidad, e.estadocivil,m.idempresa,m.nombree, m.nombrerep,m.gradosup , m.cargorep ,m.areappp , m.telefono,m.fechappp  , m.direccion from empresa m inner join estudiante e  on e.idestudiante  = m.idestudian inner join persona p on p.idpersona = e.idperso  ");
   
       return res.status(200).json({
         status: true,
