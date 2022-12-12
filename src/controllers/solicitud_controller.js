002b19d611db6a55383723d54be85c11b665ec1d
ChristianHumpiri1 <christianquispe@upeu.edu.pe>
2022-12-12 17:49:41 -0500

    id

--- src/controllers/solicitud_controller.js
+++ src/controllers/solicitud_controller.js
@@ -5,7 +5,8 @@ const solicitudctr = {};
 
 solicitudctr.getAllsolicitud = async (req, res) => {
     try {
-      const response = await pool.query("select  p.idpersona ,p.nombre, concat(p.apepat,' ', p.apemat) as Apellidos,p.dni, p.codigo, e.idestudiante ,e.telefono as telefonoe , e.ubigeo, e.nacionalidad, e.estadocivil,m.idempresa,m.nombree, m.nombrerep,m.gradosup , m.cargorep ,m.areappp , m.telefono,m.fechappp  , m.direccion from empresa m inner join estudiante e  on e.idestudiante  = m.idestudian inner join persona p on p.idpersona = e.idperso  ");
+      
+      const response = await pool.query("select  p.idpersona ,p.nombre, concat(p.apepat,' ', p.apemat) as Apellidos,p.dni, p.codigo, e.idestudiante ,e.telefono as telefonoe , e.ubigeo, e.nacionalidad, e.estadocivil,m.idempresa,m.nombree, m.nombrerep,m.gradosup , m.cargorep ,m.areappp , m.telefono,m.fechappp  , m.direccion from empresa m inner join estudiante e  on e.idestudiante  = m.idestudian inner join persona p on p.idpersona = e.idperso ");
 
       const responseDocumeto = await pool.query("select * from documento");
 
@@ -25,16 +26,28 @@ solicitudctr.getAllsolicitud = async (req, res) => {
     }
   };
 
-//   solicitudctr.readsolicitud = async(req, res) => {
-//     try {
-//         const id = parseInt(req.params.id);
-//         const response = await pool.query('select pa.idpaciente, p.nombres, p.apellidos, p.telefono, p.pais, p.correo, pa.motivoconsulta from paciente pa, persona p, psicologos ps where pa.idpsicologo = $1 and pa.idpersona=p.idpersona and pa.idpsicologo = ps.idpsicologo;', [id]);
-//         return res.status(200).json(response.rows);
-//     } catch (e) {
-//         console.log(e);
-//         return res.status(500).json('Internal Server error...!');
-//     }
-// }
+  solicitudctr.getAllsolicitudid = async (req, res) => {
+    try {
+      const id = parseInt(req.params.id);
+      const response = await pool.query("select  p.idpersona ,p.nombre, concat(p.apepat,' ', p.apemat) as Apellidos,p.dni, p.codigo, e.idestudiante ,e.telefono as telefonoe , e.ubigeo, e.nacionalidad, e.estadocivil,m.idempresa,m.nombree, m.nombrerep,m.gradosup , m.cargorep ,m.areappp , m.telefono,m.fechappp  , m.direccion from empresa m inner join estudiante e  on e.idestudiante  = m.idestudian inner join persona p on p.idpersona = e.idperso where e.idestudiante = $1 ", [id]);
+      const responseDocumeto = await pool.query("select * from documento");
+
+      return res.status(200).json({
+        status: true,
+        resp: "Ok",
+        message: "Se obtuvo las solicitudes",
+        data: response.rows,
+        documents: responseDocumeto.rows
+      });
+    } catch (error) {
+      return res.status(400).json({
+        status: true,
+        resp: "Ok",
+        message: error.messages
+      });
+    }
+  };
+
 
 solicitudctr.eliminarSolicitud = async(req, res) => {
   try {
