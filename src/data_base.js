53aac5ff2f78cae1c8c0efd6f6103d56661bf67c
ChristianHumpiri1 <christianquispe@upeu.edu.pe>
2022-12-15 15:33:25 -0500

    db

--- src/data_base.js
+++ src/data_base.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    host: 'ec2-3-216-167-65.compute-1.amazonaws.com',
-    user: 'gteiyscefprcgx',
-    password: 'cbf409d0c595bc2657d24a0d36d7561ade3773743863df91b439dc02aa89eb89',
-    database: 'dee4os3atfjhkj',
+    host: 'ec2-3-225-213-67.compute-1.amazonaws.com',
+    user: 'yldvcbwdpwvyyb',
+    password: '7c3717fbf633c58af9e7dc596586f2257602245cc22e14a0beab9351ac4e68f2',
+    database: 'dce8i9oub03tbd',
     port: 5432,
     ssl: { rejectUnauthorized: false }
 })
