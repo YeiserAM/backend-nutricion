9e7e430d1a924b267135e43e7c83303b9e7d7e59
yeiser <yeiseravila@upeu.edu.pe>
2022-12-19 23:20:42 -0500

    uuu

--- src/data_base.js
+++ src/data_base.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    host: 'bxjfyeaysrvjslyk5kqm-postgresql.services.clever-cloud.com',
-    user: 'uxfvq8owbmmevzo3ak0d',
-    password: 'DC5ywzTH6Bm63rlYoAXv',
-    database: 'bxjfyeaysrvjslyk5kqm',
+    host: 'ec2-52-3-60-53.compute-1.amazonaws.com',
+    user: 'ymwjzplxmgagpo',
+    password: '940beb3583db5ec3d8df450ee0e5d59a66a465a3020375cee2342b8825143619',
+    database: 'dast13dufsnsof',
     port: 5432,
     ssl: { rejectUnauthorized: false }
 });
